326dd2bdaa6aace1bcb35313c1dc6a462f48a221
Lakshmi <Lakshmi@NTPLAP-008.chn.nestgroup.net>
2022-10-17 15:07:08 +0530

    push1

--- src/main/java/com/nestdigital/PostMethodApi/controller/PostMethod.java
+++ src/main/java/com/nestdigital/PostMethodApi/controller/PostMethod.java
@@ -4,8 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
+import java.util.List;
 import java.util.ArrayList;
+
 
 @RestController
 
@@ -18,5 +19,14 @@ public class PostMethod {
     public String Homepage() {
         return "Welcome to home page";
     }
-
+    @GetMapping("/viewall")
+    public List Viewall(){
+        List<String> myList=new ArrayList<>();
+        myList.add("{'name':prabha,'age':20}");
+        return myList;
+    }
+    @GetMapping("/name")
+public String name(){
+        return "Prabha";
+}
 }

3313ed882b71073074ac0b261c5c354a2e9e408b
Lakshmi <Lakshmi@NTPLAP-008.chn.nestgroup.net>
2022-10-17 10:35:59 +0530

    push

---
+++ src/main/java/com/nestdigital/PostMethodApi/PostMethodApiApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.PostMethodApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostMethodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostMethodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/PostMethodApi/PostMethodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.PostMethodApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostMethodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
